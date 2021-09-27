10b1f66a2bdfecb55359ea8f308040d9c0f5b391
GolamMuktadirHossainSaber <golammuktadirhossain18@gmail.com>
2021-09-27 21:52:33 +0600

    c-5

--- src/componands/Header/Header.js
+++ src/componands/Header/Header.js
@@ -4,10 +4,10 @@ import React from 'react';
 const Header = () => {
     return (
 
-        <div className=" p-4 rounded-3 mb-4 " style={{ 'backgroundColor': 'rgb(253, 236, 235)' }}>
+        <div className=" p-4 rounded-3 mb-4 " style={{ 'backgroundColor': 'rgb(216 236 255)' }}>
             <div className="thisItem" >
-                <h1>National <span className="text-warning">Hackathon</span> Organization</h1>
-                <h5><em>Choose Guest For 'Summer Hackathon 2021'</em></h5>
+                <h1> <span className="text-primary">Hero Learner's</span> Organization</h1>
+                <h5><em>Choose Guest For next 'Programming Summit  2021'</em></h5>
                 <h1>Total Budget: 1000 Million</h1>
             </div>
         </div>
